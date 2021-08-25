bff0d12decffbbe4e84bf3ac5a02ec70c88d2a95
ZinKoWinn <61304140+ZinKoWinn@users.noreply.github.com>
2021-08-25 15:08:40 +0630

    first commit

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,25 +1,67 @@
 $(document).ready(function() {
-    if (!Modernizr.meter) {
-        alert('Sorry your brower does not support HTML5 progress bar');
-    } else {
-        var progressbar = $('#progressbar'),
-            max = progressbar.attr('max'),
-            time = (1000 / max) * 5,
-            value = progressbar.val();
 
-        var loading = function() {
-            value += 1;
-            addValue = progressbar.val(value);
+    // Sticky Navigation
+    $('.js-section-about-me').waypoint(function(direction) {
+        if (direction == 'down') {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '150px'
+    });
 
-            $('.progress-value').html(value + '%');
 
-            if (value == max) {
-                clearInterval(animate);
+    // Mobile Navigation
+
+    $('.navbar').click(function() {
+        alert("I am an alert box!");
+        $('.js-navbar').slideToggle(200);
+
+        if ($('.js-nav-icon i').hasClass('fa-bars')) {
+            $('.js-nav-icon i').addClass('fa-times');
+            $('.js-nav-icon i').removeClass('fa-bars');
+        } else {
+            $('.js-nav-icon i').addClass('fa-bars');
+            $('.js-nav-icon i').removeClass('fa-times');
+        }
+    });
+
+
+    // Scrool Navigation Button Effect
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function() {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
             }
-        };
+        });
 
-        var animate = setInterval(function() {
-            loading();
-        }, time);
-    };
 });
